4fef109a52ed2734abfaef000b993fc571f0ce78
Francesco Witte <francesco.witte@dlr.de>
2023-11-10 13:08:46 +0100

    Add legend label to bar plot

--- tabs/tab_tab0.py
+++ tabs/tab_tab0.py
@@ -11,18 +11,25 @@ def create(data, metadata, cfg):
     metadata = metadata["/Scalars/ScenTechNode"]
 
     config = {"selector" : "Node", "grouper": "Scenario"}
+    col = "ElectricalInstalledCapacities"
     select = st.selectbox(
         label=f"Select {config['selector']}",
         options=data.index.get_level_values(config["selector"]).unique().tolist(),
     )
 
-    df = data.query(f"Node == '{select}'")
+    df = data.query(f"Node == '{select}'")[[col]].round()
     df.index = df.index.droplevel("Node")
     df.reset_index(inplace=True)
     bar_options = barplot_grouped(
-        df[["Scenario", "ElectricalInstalledCapacities", "Technology"]],
+        df[["Scenario", col, "Technology"]],
         metadata
     )
     bar_options = update_options_with_defaults(bar_options)
-    # line_options = update_options_with_user_overrides(line_options, plotoptions)
-    st_echarts(bar_options)
+
+    options_update = {
+        'yAxis': {
+            'name': f'{metadata[col]["label"]} in {metadata[col]["unit"]}'
+        }
+    }
+    bar_options = update_options_with_user_overrides(bar_options, options_update)
+    st_echarts(bar_options, height="500px", width="95%")
